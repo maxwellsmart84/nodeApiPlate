55ca507b8673ebd7db32d646aa6c80fe65fc5abb
Maxwell Krause <maxwell.l.krause@gmail.com>
2018-03-30 00:10:24 -0400

    added dotenv

--- src/serve.js
+++ src/serve.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
